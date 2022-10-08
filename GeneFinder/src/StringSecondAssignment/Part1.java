ac519f1f4af98f9dc8ce25a8a9aae8b9f8ee4757
Zhuravel <zhzhuravel@gmail.com>
2022-10-08 21:47:23 +0200

    Add findStopCodon() method to Part1 class
    findStopCodon() method is used for finding last codon index.
    Additionally,were added test cases in Runner class

--- GeneFinder/src/StringSecondAssignment/Part1.java
+++ GeneFinder/src/StringSecondAssignment/Part1.java
@@ -1,4 +1,20 @@
 package StringSecondAssignment;
 
 public class Part1 {
+
+    public static int findStopCodon(String dna, int startIndex, String stopCodon) {
+        int stopCodonIndex = 0;
+        int startCodonIndex = dna.indexOf("ATG", startIndex);
+        if (startCodonIndex != -1) {
+            while (stopCodonIndex != -1) {
+                stopCodonIndex = dna.indexOf(stopCodon, startCodonIndex);
+                if (stopCodonIndex % 3 != 0) {
+                    startCodonIndex = stopCodonIndex + 3;
+                } else {
+                    break;
+                }
+            }
+        }
+        return stopCodonIndex;
+    }
 }
--- GeneFinder/src/StringSecondAssignment/Runner.java
+++ GeneFinder/src/StringSecondAssignment/Runner.java
@@ -3,6 +3,6 @@ package StringSecondAssignment;
 public class Runner {
 
     public static void main(String[] args) {
-
+        System.out.println(Part1.findStopCodon("ATGTTAGTTAATTAA", 0, "TAA"));
     }
 }
