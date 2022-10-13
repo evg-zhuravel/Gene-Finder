05bb6b2ebebba7d1acffc415dbf1066f8b8bd9ee
Zhuravel <zhzhuravel@gmail.com>
2022-10-13 16:27:45 +0200

    Add testCode in bugfix1 branch
    Added countGenes() method in Part1 class. Also trying to fix problems with
    findGene() method, wich is not capable to find gene wich is not start with
    ATG codon.

--- GeneFinder/src/StringSecondAssignment/Part1.java
+++ GeneFinder/src/StringSecondAssignment/Part1.java
@@ -1,9 +1,12 @@
 package StringSecondAssignment;
 
+import java.util.Arrays;
+
 public class Part1 {
 
     public static int findStopCodon(String dna, int startIndex, String stopCodon) {
         int stopCodonIndex = 0;
+        dna = dna.substring(startIndex);
         int startCodonIndex = dna.indexOf("ATG", startIndex);
         if (startCodonIndex != -1) {
             while (stopCodonIndex != -1) {
@@ -22,14 +25,41 @@ public class Part1 {
         StringBuilder gene = new StringBuilder();
         int startCodonIndex = dna.indexOf("ATG");
         if (startCodonIndex != -1) {
-            String[] stopCodons = {"TAA", "TAG", "TGA"};
-            for (String stopCodon : stopCodons) {
-                int currIndex;
-                if ((currIndex = findStopCodon(dna, startCodonIndex, stopCodon)) > 0 && gene.isEmpty()) {
-                    gene.append(dna, startCodonIndex, currIndex + 3);
-                }
-            }
+            int firstStopCodonIndex = findMinimalIndex(dna, startCodonIndex, "TAA", "TAG", "TGA");
+            gene.append(dna, startCodonIndex, firstStopCodonIndex + 3);
         }
         return gene.toString();
+    }
+
+    private static int findMinimalIndex(String dna, int startIndex, String... stopCodons) {
+        int minimalCodon = 0;
+        int[] lastIndexes = new int[stopCodons.length];
+
+        for (int i = 0; i < stopCodons.length; i++) {
+            lastIndexes[i] = findStopCodon(dna, startIndex, stopCodons[i]);
+        }
+
+        Arrays.sort(lastIndexes);
+
+        for (int index : lastIndexes) {
+            if (minimalCodon == 0 && index != -1) {
+                minimalCodon = index;
+            }
+        }
+        return minimalCodon;
+    }
+
+    public static int countGenes(String dna) {
+        int count = 0;
+        while (!dna.isEmpty()) {
+            String currGene = findGene(dna);
+            if (!currGene.isEmpty()) {
+                count++;
+                dna = dna.substring(currGene.length());
+            } else {
+                break;
+            }
+        }
+        return count;
     }
 }
--- GeneFinder/src/StringSecondAssignment/Runner.java
+++ GeneFinder/src/StringSecondAssignment/Runner.java
@@ -3,14 +3,13 @@ package StringSecondAssignment;
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println(Part1.findStopCodon("ATGTTAGTTAATTAA", 0, "TAA"));
-
         String testDna1 = "ATGTTAGTTAATTAA"; // res: ATGTTAGTTAATTAA
         String testDna2 = "TTTGTTAATTAA"; // res:
         String testDna3 = "ATGTAAGTTAATTAA"; // res: ATGTAA
         String testDna4 = "ATGTGGTTAATTTGAAAT"; // res: ATGTGGTTAATTTGA
         String testDna5 = "TGAATGGCTGATAAGTAGGTTTGA"; // res: ATGGCTGATAAGTAG
-        String testDna6 = "TGAATGGCTGATAAGTTAGGTTTGA"; // res:
+        String testDna6 = "ATGAGTTAGTAAATAA"; // res: ATGAGTTAG
+
 
         System.out.println(Part1.findGene(testDna1));
         System.out.println(Part1.findGene(testDna2));
@@ -18,5 +17,10 @@ public class Runner {
         System.out.println(Part1.findGene(testDna4));
         System.out.println(Part1.findGene(testDna5));
         System.out.println(Part1.findGene(testDna6));
+
+        System.out.println(Part2.howMany("GAA", "ATGAACGAATTGAATCGAAGAAGAA")); // 6
+        System.out.println(Part2.howMany("AA", "ATAAAA")); // 2
+
+        System.out.println(Part1.countGenes("ATGTAAGATGCCCTAGTATGTTAGTTAATTAA")); // 3
     }
 }
