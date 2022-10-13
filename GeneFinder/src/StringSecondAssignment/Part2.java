6534860dfb63fe4a3626397a4eddd907f7dd7130
Zhuravel <zhzhuravel@gmail.com>
2022-10-13 12:50:07 +0200

    Add howMany() method in Part2 class
    This method can calculate a count how many times a string is appeared in
    another string. In addition, added a few test cases in Runner class

--- GeneFinder/src/StringSecondAssignment/Part2.java
+++ GeneFinder/src/StringSecondAssignment/Part2.java
@@ -1,5 +1,14 @@
 package StringSecondAssignment;
 
 public class Part2 {
-    
+
+    public static int howMany(String stringa, String stringb) {
+        int counter = 0;
+        while (stringb.contains(stringa)) {
+            counter++;
+            int currIndex = stringb.indexOf(stringa);
+            stringb = stringb.substring(currIndex + stringa.length());
+        }
+        return counter;
+    }
 }
